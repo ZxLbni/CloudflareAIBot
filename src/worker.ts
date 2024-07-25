fa03bb8779b015216d4e52abb8a7e84fb0fe948b
⤷ßñï 🪽 <101978891+ZxLbni@users.noreply.github.com>
2024-07-25 17:18:44 +0530

    Update worker.ts

--- src/worker.ts
+++ src/worker.ts
@@ -22,7 +22,7 @@ export default {
 					if (obj.hasOwnProperty('message')) {
 						if (obj.message.hasOwnProperty('text')) {
 							if (obj.message.chat.type == "private") {
-								let message = "The Bot works in this Group: https://telegram.dog/+aZtULiBLkSMwOGY1"
+								let message = "This Bot works in this Group: https://t.me/+qjcyUowArYYzNDU9"
 								await fetch("https://api.telegram.org/bot" + tg_bot_token + "/SendMessage?disable_web_page_preview=true&chat_id=" + obj.message.chat.id + "&reply_to_message_id=" + obj.message.message_id + "&text=" + message, {
 									method: "GET"
 								});
